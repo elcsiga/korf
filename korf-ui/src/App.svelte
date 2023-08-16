2ed57e29a5119eb6439f76066f99d2dc48920e41
andras <andras@cloudera.com>
2023-08-16 15:15:20 +0200

    nav

--- korf-ui/src/App.svelte
+++ korf-ui/src/App.svelte
@@ -1,26 +1,5 @@
 <script lang="ts">
-  import { appState } from "./state/state";
-  import { Alert } from 'flowbite-svelte';
-
-
+  import Workstation from "./clients/workstation/Workstation.svelte";
 </script>
 
-<main>
-  <h1>KORF 3</h1>
-
-
-  <div class="p-8">
-    <Alert>
-      <span class="font-medium">Info alert!</span>
-      Change a few things up and try submitting again.
-    </Alert>
-  </div>
-
-  { JSON.stringify($appState) }
-
-</main>
-
-<style>
-
-
-</style>
+<Workstation />
---
+++ korf-ui/src/clients/workstation/Workstation.svelte
@@ -0,0 +1,34 @@
+<script>
+  import { NavBrand, NavHamburger, NavLi, NavUl, Navbar, SpeedDial, SpeedDialButton } from "flowbite-svelte";
+  import { Icon } from "flowbite-svelte-icons";
+  import { appState } from "../../state/state";
+  import { Alert, DarkMode } from "flowbite-svelte";
+</script>
+
+<Navbar let:hidden let:toggle>
+  <NavBrand href="/">
+    <img
+      src="korf.webp"
+      class="mr-3 h-6 sm:h-9"
+      alt="Korfball"
+    />
+    <span
+      class="self-center whitespace-nowrap text-xl font-semibold dark:text-white"
+      >Korf</span
+    >
+  </NavBrand>
+  <NavHamburger on:click={toggle} />
+  <NavUl {hidden}>
+    <NavLi href="/" active={true}>Home</NavLi>
+    <NavLi href="/about">Teams</NavLi>
+    <NavLi href="/services">Players</NavLi>
+    <NavLi href="/pricing">Matches</NavLi>
+    <NavLi href="/contact">Contact</NavLi>
+  </NavUl>
+</Navbar>
+
+<DarkMode />
+<Alert>
+  <span class="font-medium">Info alert!</span>
+  Change a few things up and try submitting again.
+</Alert>
